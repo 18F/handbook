21e3a269b924d282f5f88ce84540f6b5e3147096
Owen Barton <owen.barton@civicactions.com>
2017-03-16 14:10:44 -0700

    First pass at Travis/gulp build.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp");
+var through2 = require("through2");
+var markdownlint = require("markdownlint");
+
+gulp.task("markdownlint", function task() {
+  return gulp.src(["**/*.md", "!node_modules/**"], { "read": false })
+    .pipe(through2.obj(function obj(file, enc, next) {
+      markdownlint(
+        { "files": [ file.relative ],
+          "config": {
+            // Enter config for markdownlint below:
+            // See https://github.com/DavidAnson/markdownlint#optionsconfig
+            // and https://github.com/DavidAnson/markdownlint/tree/master/style
+            "default": true,
+            "line_length": false
+          }
+        },
+        function callback(err, result) {
+          var resultString = (result || "").toString();
+          if (resultString) {
+            console.log(resultString);
+          }
+          next(err, file);
+        });
+    }));
+});
+
+gulp.task('default', ['markdownlint']);
