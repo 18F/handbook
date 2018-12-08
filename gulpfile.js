d0ea192a8ce7a8b72251de5a1fd0e39fc2b3b035
Owen Barton <owen.barton@civicactions.com>
2018-12-07 19:27:26 -0800

    Re-enable warnings (renamed to suggestions) that are limited to only changed files, and more carefully labeled.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require("gulp");
 var remark = require('gulp-remark');
 var options = require('gulp-options');
+var log = require('fancy-log');
+var execSync = require('child_process').execSync;
+var stream = require('stream');
 
 // Stream writer that changes warnings to errors
-var stream = require('stream');
 var errorStream = new stream.Writable();
 errorStream._write = function (chunk, encoding, done) {
   console.log(chunk.toString().replace(/warnings/g, "\x1b[31merrors\x1b[0m").replace(/warning/g, "\x1b[31merror\x1b[0m"));
+  done();
+};
+
+// Stream writer that changes warnings to suggestions
+var suggestionStream = new stream.Writable();
+suggestionStream._write = function (chunk, encoding, done) {
+  console.log(chunk.toString().replace(/warnings/g, "suggestions").replace(/warning/g, "suggestion"));
   done();
 };
 
@@ -18,27 +27,39 @@ gulp.task('remark', function () {
   if (options.has('path')) {
     path = options.get('path');
   }
-  return gulp.src(path)
+  gulp.src(path)
     // Main run against the "error" configuration.
+    .on('end', function () { log("\x1b[31mFailing issues:\x1b[0m") })
     .pipe(remark(
-      { 
+      {
         "quiet": true,
         "streamError": errorStream,
         "color": true,
         "rcPath": ".remarkrc.error"
-      }))
-    // Save any changes.
-    // .pipe(gulp.dest('.'))
-    // Finally, run against "warning" configuration.
+      }));
 
-    // Temporarily disabled because retext is not very helpful yet. 
-    // .pipe(remark(
-    //     {
-    //         "quiet": true,
-    //         "streamError": process.stdout,
-    //         "color": true,
-    //         "rcPath": ".remarkrc.warning"
-    //     }));
+  // Run against "warning" configuration for files changed from master only to reduce noise.
+  var changedFiles = execSync('git diff --name-status master').toString().split('\n');
+  for (var i = 0; i < changedFiles.length; i++) {
+    if (changedFiles[i]) {
+      var statusFile = changedFiles[i].split('\t');
+      if (statusFile[0] == 'D') {
+        continue;
+      }
+      if (statusFile[1].split('.').pop() != 'md') {
+        continue;
+      }
+      gulp.src(statusFile[1])
+        .on('end', function () { log("\x1b[32mSuggestion for changed file:\x1b[0m") })
+        .pipe(remark(
+          {
+            "quiet": true,
+            "streamError": suggestionStream,
+            "color": true,
+            "rcPath": ".remarkrc.suggestion"
+          }));
+    }
+  }
 });
 
 gulp.task('default', ['remark']);
