7de6e5acc5c1a9d3e71f158b082bfcdb2d0fe6a2
Travis CI <travis@travis-ci.org>
2019-04-19 15:27:43 -0700

    Redirect output on initial run.

--- .push-fixes.sh
+++ .push-fixes.sh
@@ -1,4 +1,6 @@
-gulp
+# Run once to fix content as needed - redirect output to avoid double output
+gulp &> /dev/null
+# Commit and push any changes
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git commit -a -m"Automatic commit to fix markdown formatting (Build: ${TRAVIS_BUILD_NUMBER})."
