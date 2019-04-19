e3bec5b86a4ad857ae03b8cd913ba554e5d47e12
Travis CI <travis@travis-ci.org>
2019-04-19 15:35:30 -0700

    Add check for loops and git status.

--- .push-fixes.sh
+++ .push-fixes.sh
@@ -1,8 +1,16 @@
+if git log -1 --pretty=%B | grep -Fq 'Automatic commit'; then
+  echo "Previous commit was automatic, so skip pushing fixes.'
+  exit 0
+fi
+
 # Run once to fix content as needed - redirect output to avoid double output
 gulp &> /dev/null
+
 # Commit and push any changes
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git commit -a -m"Automatic commit to fix markdown formatting (Build: ${TRAVIS_BUILD_NUMBER})."
-git remote add pr "https://${GH_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}"
-git push pr "HEAD:${TRAVIS_PULL_REQUEST_BRANCH}"
+if [[ `git status --porcelain` ]]; then
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git commit -a -m"Automatic commit to fix markdown formatting (Build: ${TRAVIS_BUILD_NUMBER})."
+  git remote add pr "https://${GH_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}"
+  git push pr "${TRAVIS_PULL_REQUEST_BRANCH}"
+fi
