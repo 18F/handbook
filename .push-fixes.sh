e1dbb5132757c4ac30cdc7696333a16865f27927
Travis CI <travis@travis-ci.org>
2019-04-19 15:39:16 -0700

    Making exit code tests consistent.

--- .push-fixes.sh
+++ .push-fixes.sh
@@ -1,4 +1,4 @@
-if git log -1 --pretty=%B | grep -Fq 'Automatic commit'; then
+if [[ $(git log -1 --pretty=%B | grep -Fq 'Automatic commit') ]]; then
   echo "Previous commit was automatic, so skip pushing fixes.'
   exit 0
 fi
@@ -7,10 +7,10 @@ fi
 gulp &> /dev/null
 
 # Commit and push any changes
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git commit -a -m"Automatic commit to fix markdown formatting (Build: ${TRAVIS_BUILD_NUMBER})."
+  git commit -a -m"Automatic commit to fix markdown formatting for build: ${TRAVIS_BUILD_NUMBER}."
   git remote add pr "https://${GH_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}"
   git push pr "${TRAVIS_PULL_REQUEST_BRANCH}"
 fi
