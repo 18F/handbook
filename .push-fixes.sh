a366eca794407f4770c7753961396bd35202af25
Travis CI <travis@travis-ci.org>
2019-04-19 15:13:08 -0700

    Add remote for pushing

--- .push-fixes.sh
+++ .push-fixes.sh
@@ -2,4 +2,5 @@ gulp
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git commit -a -m"Automatic commit to fix markdown formatting (Build: ${TRAVIS_BUILD_NUMBER})."
-git push origin HEAD:"${TRAVIS_PULL_REQUEST_BRANCH}"
+git add remote pr "https://${GH_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}"
+git push pr "HEAD:${TRAVIS_PULL_REQUEST_BRANCH}"
