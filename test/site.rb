cba8002c7b431751c08eda9a8eae403d367b4053
Aidan Feldman <aidan.feldman@gsa.gov>
2020-05-28 03:18:03 -0400

    sidestep checking for the baseurl in tests

--- test/site.rb
+++ test/site.rb
@@ -1,27 +1,25 @@
 require "nokogiri"
 require "test/unit"
 
-BASEURL = "http://0.0.0.0:4000/"
-
 class SiteTest < Test::Unit::TestCase
   def test_redirect
-    dest_url = "#{BASEURL}tech-portfolio/"
+    dest = "/tech-portfolio/"
     doc = File.open("_site/infrastructure/index.html") { |f| Nokogiri::HTML(f) }
 
     link = doc.at_css('link[rel="canonical"]')
     assert link
-    assert_equal dest_url, link["href"]
+    assert link["href"].include?(dest)
 
     meta = doc.at_css('meta[http-equiv="refresh"]')
     assert meta
-    assert meta["content"].include?("url=#{dest_url}")
+    assert meta["content"].include?(dest)
   end
 
   def test_sitemap
     doc = File.open("_site/sitemap.xml") { |f| Nokogiri::XML(f) }
 
     doc.css("urlset url loc").each do |loc|
-      assert loc.content.start_with?(BASEURL)
+      assert loc.content.start_with?("http")
     end
   end
 end
