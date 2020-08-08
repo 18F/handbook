9c92fdcd5526d19d1f3d07dcb5fccb75ca4fcdb6
Aidan Feldman <aidan.feldman@gsa.gov>
2020-08-08 07:11:01 -0400

    get the software search working

--- javascripts/software-search.js
+++ javascripts/software-search.js
@@ -19,17 +19,60 @@ const buildIndex = (software) => {
   });
 };
 
-const getIndex = async () => {
-  const software = await getSoftware();
-  return buildIndex(software);
+// https://medium.com/dailyjs/rewriting-javascript-converting-an-array-of-objects-to-an-object-ec579cafbfc7
+const arrayToObject = (array, keyField) =>
+  array.reduce((obj, item) => {
+    obj[item[keyField]] = item;
+    return obj;
+  }, {});
+
+const displayResults = (software) => {
+  console.log(software);
+  const table = document.querySelector("#software-search-results tbody");
+
+  // empty it
+  table.innerHTML = "";
+
+  software.forEach((entry) => {
+    const row = table.insertRow();
+    const th = document.createElement("th");
+    th.setAttribute("scope", "row");
+    const text = document.createTextNode(entry["Standard Name"]);
+    th.appendChild(text);
+    row.appendChild(th);
+
+    const fields = [
+      "Description",
+      "Category",
+      "Status",
+      "Deployment Type",
+      "Approval Expiration Date",
+    ];
+    fields.forEach((field) => {
+      const cell = row.insertCell();
+      const text = document.createTextNode(entry[field]);
+      cell.appendChild(text);
+    });
+  });
 };
 
-const indexPromise = getIndex();
+const doSearch = (query, index, softwareByName) => {
+  const nResults = 5;
+  const searchResults = index.search(query).slice(0, nResults);
+  // retrieve the matching software entries
+  const softwareResults = searchResults.map(
+    (result) => softwareByName[result.ref]
+  );
+  displayResults(softwareResults);
+};
 
-const input = document.querySelector('#software-search input[name="search"]');
-input.addEventListener("input", async (event) => {
-  indexPromise.then((index) => {
-    const results = index.search(event.target.value);
-    console.log(results);
+getSoftware().then((software) => {
+  const index = buildIndex(software);
+  const softwareByName = arrayToObject(software, "Standard Name");
+
+  const input = document.querySelector('#software-search input[name="search"]');
+  input.addEventListener("input", async (event) => {
+    const query = event.target.value;
+    doSearch(query, index, softwareByName);
   });
 });
