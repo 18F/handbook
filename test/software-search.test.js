d4d78ee39f2014f0db915f9290303f88b7c74da9
Aidan Feldman <aidan.feldman@gsa.gov>
2020-08-11 03:25:06 -0400

    ensure results are in descending order

--- test/software-search.test.js
+++ test/software-search.test.js
@@ -24,7 +24,7 @@ test("matches a partial word", () => {
   expect(results.length).toBe(1);
 });
 
-test("matches a single word within the name", () => {
+test("results are in descending order", () => {
   const index = search.buildIndex([
     {
       "Standard Name": "something something Git",
@@ -38,5 +38,21 @@ test("matches a single word within the name", () => {
 
   const results = index.search("git");
   expect(results.length).toBe(2);
+  expect(results[0].score).toBeGreaterThan(results[1].score);
+});
+
+test("matches a single word within the name", () => {
+  const index = search.buildIndex([
+    {
+      "Standard Name": "something something Git",
+      Description: "foo bar",
+    },
+    {
+      "Standard Name": "something something",
+      Description: "This one is also Git.",
+    },
+  ]);
+
+  const results = index.search("git");
   expect(results[0].ref).toBe("something something Git");
 });
