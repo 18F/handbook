affbb57eaea1f5f718b467136047862abb40a809
LauraGGerhardt <laura.gerhardt@gsa.gov>
2023-01-03 15:22:39 -0500

    convert to inpage nav

--- config/postbuild/buildSidenavs.js
+++ config/postbuild/buildSidenavs.js
@@ -14,7 +14,7 @@ const buildSidenavs = async (path, dom, verbose = false) => {
 
   if (!isFrontPage && !hasNav) {
     // Only traverse down to h3 so the table of contents is not unwieldy
-    const headings = document.querySelectorAll("main > h2, main > h3");
+   const headings = document.querySelectorAll("main > h2, main > h3");
 
     if (headings.length) {
       log(path);
@@ -25,50 +25,19 @@ const buildSidenavs = async (path, dom, verbose = false) => {
         [body.getAttribute("class"), "with-inline-navigation"].join(" ")
       );
 
-      const nav = document.createElement("nav");
-      nav.setAttribute("role", "navigation");
-      nav.setAttribute("aria-label", "Side navigation,");
-      nav.setAttribute("class", "sticky-side tablet:grid-col-4");
+      const inPageNav = document.createElement("aside");
+      inPageNav.setAttribute("class", "usa-in-page-nav");
+      inPageNav.setAttribute("data-title-text", "On this page");
+      inPageNav.setAttribute("data-title-heading-level", "h3");
+      inPageNav.setAttribute("data-scroll-offset", "0");
+      inPageNav.setAttribute("data-root-margin","0px 0px 0px 0px");
+      inPageNav.setAttribute("data-threshold", "1");
+  
+      document.querySelector("main").parentNode.setAttribute(
+        "class", "usa-in-page-nav-container")
+      document.querySelector("main").parentNode.insertBefore(inPageNav, document.querySelector("main"));
 
-      const h4 = document.createElement("h4");
-      h4.textContent = "On this page:";
-      nav.appendChild(h4);
-
-      const list = document.createElement("ul");
-      nav.appendChild(list).setAttribute("class","usa-sidenav");
-
-      for (const heading of headings) {
-        // Remove any heading permalink content. Our 11ty build adds permalinks
-        // to all headers in the main body of content. Those links are just a
-        // link 🔗 icon. Those icons should NOT show up in the floating
-        // navigation sidebar, so... we'll just throw them away.
-        //
-        // But do that on a clone because we don't mess up the real DOM.
-        const clone = heading.cloneNode(true);
-        for (const child of clone.querySelectorAll(".heading-permalink")) {
-          child.remove();
-        }
-
-        const title = clone.textContent.trim();
-        const id = heading.getAttribute("id");
-        
-          const item = document.createElement("li");
-          item.setAttribute("class", "usa-sidenav__item");
-
-          if (heading.tagName.toLowerCase() == "h3") {
-            item.setAttribute("class", "usa-sidenav__item sub-child");
-          }
-          item.innerHTML = `<a href="#${id}">${title}</a>`;
-        
-        list.appendChild(item);
-      }
-        // insert node after
-        document.querySelector("main").parentNode.insertBefore(nav, document.querySelector("main").nextSibling);
-        document.querySelector("main").setAttribute("class", "usa-prose tablet:grid-col-8");
-      return true;
     }
-  } else {
-    document.querySelector("main").setAttribute("class", "usa-prose width-tablet-lg");
   }
 
   return false;
